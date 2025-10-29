5d068e44d53f68aa4428237fa328e08529d554c0
Ayush Mehta <ayushmehtabps@gmail.com>
2025-10-29 12:35:07 +0530

    clerk-auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image"
 import logo from "../public/logo/logo.png"
+import { ModeToggle } from "./ui/ModeToggle"
+import { SignUp } from "@clerk/nextjs"
 
 Image
 const Navbar = () =>{
   return (
-    <div className='flex items-center p-1 m-1 '>
+    <div className='flex items-center p-1 m-1 justify-between '>
     <div className="gap-2 flex items-center  ">
         <Image src={logo} width={120} height={100} alt="logo" />
+
+    </div>
+
+    <div className="flex items-center gap-3 m-2 p-1 ">
+      <ModeToggle />
+      
 
     </div>
 
